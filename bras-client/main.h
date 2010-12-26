0d50b619475806a058400feb3bad157369c49842
Kitten <zcbenz@gmail.com>
2010-12-26 22:45:54 +0800

    add main.h

---
+++ bras-client/main.h
@@ -0,0 +1,29 @@
+#include <gtkmm/main.h>
+#include <gtkmm/messagedialog.h>
+#include <gtkmm/image.h>
+
+#include <unistd.h>
+
+#include "bras.h"
+#include "logindlg.h"
+
+namespace Gtk {
+    enum MY_RESPONESE {
+        RESPONSE_DISCONNECT = 1,
+        RESPONSE_CONNECT
+    }; 
+}
+
+typedef void (*state_func_t)();
+
+static bool on_bras_state_change(Glib::IOCondition io_condition);
+static void on_close();
+static void on_quit();
+static void on_login();
+static void on_connecting();
+static void on_connected();
+static void on_using();
+static void on_error();
+static void on_dlg_response(int response);
+
+static bool on_brasd_no_response();
