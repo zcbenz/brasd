1e4b61dde10b0afece2192dc962f27cad368ff66
Kitten <zcbenz@gmail.com>
2011-01-18 23:24:15 +0800

    add iterator support for Options

--- bras-client/options.h
+++ bras-client/options.h
@@ -8,17 +8,22 @@ class Options {
 public:
     static Options *get();
 
+    typedef std::map<Glib::ustring, Glib::ustring> Passwords;
+    typedef Passwords::iterator iterator;
+    typedef Passwords::const_iterator const_iterator;
+
     static Glib::ustring get_username() { return curt_; }
     static Glib::ustring get_password() { return get_password(curt_); }
     static Glib::ustring get_password(const Glib::ustring& username) { return passwords_[username]; }
     static void add_passwd(const Glib::ustring& username, const Glib::ustring& password) { passwords_[username] = password; curt_ = username; }
 
+    static iterator begin() { return passwords_.begin(); }
+    static iterator end() { return passwords_.end(); }
+
 private:
     Options();
     Options(const Options&);
     ~Options();
-
-    typedef std::map<Glib::ustring, Glib::ustring> Passwords;
 
     static Passwords passwords_;
     static Glib::ustring curt_;
